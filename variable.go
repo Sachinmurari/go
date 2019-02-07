2fc6b736e176c5aa6bfb5c0de6a2b5d8f90276e4
Sachin Murari <sachinmurari@Sachins-iMac.local>
2019-02-06 17:54:36 -0800

    variables

---
+++ variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    var (
+        name   = "naveen"
+        age    = 29
+        height int
+    )
+
+    age_1 := "sachin"
+    fmt.Println("my name", age_1)
+    fmt.Println("my name is", name, ", age is", age, "and height is", height)
+}
